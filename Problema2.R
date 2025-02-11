e63d32c996f968f815d9a60a006faa626a66ee6d
marc.lorente.palau <marc.lorente.palau@auleseebe.upc.edu>
2025-02-11 09:47:25 +0100

    clase 1 problema 2

--- Problema2.R
+++ Problema2.R
@@ -1,1 +1,29 @@
 #Codigo para problema 2
+#és un gràfic de variables discretes per varaiables contínues (barplot)
+#taula de freqüències 
+mis_dades <- mtcars
+mis_dades$cyl
+# freqüència absoluta
+ni <- table(mis_dades$cyl)
+barplot(ni)
+#freqüència relativa (probabilitats)
+fi <- ni/length(mis_dades$cyl)
+pie(fi)
+#freqüència acumulada
+Ni <- cumsum(ni)
+#freqüència relativa acumulada
+Fi <- Ni/length(mis_dades$cyl)
+Fi
+
+cbind(ni, fi, Ni, Fi)
+#taula de freqüències per contínues
+x <- mis_dades$mpg
+y <- cut(x,10)
+
+ni <- table(y)
+fi <- ni/length(y)
+Ni <- cumsum(ni)
+Fi <- Ni/length(y)
+cbind(ni, fi, Ni, Fi)
+
+hist(mis_dades$mpg)
