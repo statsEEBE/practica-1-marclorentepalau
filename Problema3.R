bcf7cd678589138565960c466b4d0088f7616558
marc.lorente.palau <marc.lorente.palau@auleseebe.upc.edu>
2025-02-11 09:55:12 +0100

    clase 1 problema 3

--- Problema3.R
+++ Problema3.R
@@ -1,1 +1,14 @@
 #Codigo para problema 3
+#farem del 1 al 20 pq fa pal copiar tot
+#es faria aixi x <- c(-8,...)
+x <- 1:30
+#a
+sum(x)
+y <- x[-c(21,2,26)]
+y
+#b
+exp(y)
+sum(exp(x))-sum(exp(y))
+#c
+y[19]
+y[24]
